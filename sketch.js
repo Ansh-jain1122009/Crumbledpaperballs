3a2d44836390610b3a9fabba6081e3dce88ade9e
Ansh-jain1122009 <86413791+Ansh-jain1122009@users.noreply.github.com>
2021-08-19 16:34:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,42 @@
+const Engine = Matter.Engine; 
+const World = Matter.World; 
+const Bodies = Matter.Bodies; 
+const Body = Matter.Body; 
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var ball,groundObj,leftSide,rightSide; 
+var world; 
+var radius = 40; 
 
-var ball;
-var ground;
-var wall;
-
-function preload()
-{
+function setup() { 
+	createCanvas(1600, 700); 
+	rectMode(CENTER); 
 	
-}
-
-function setup() {
-	createCanvas(800, 400);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0,
-		density:1.2
-	}
-	//Create the Bodies Here.
-	 ground = new Ground(width/2,390,width,20);
- 	 wall = new Ground(500,350,10,60);
-	 wall2 = new Ground(650,350,10,60);	 
-
-  	ball=Bodies.circle(100,200,20,ball_options);  
+	engine = Engine.create(); 
+	world = engine.world; 
+	
+	var ball_options={ 
+		isStatic:false, 
+		restitution:0.3, 
+		friction:0, 
+		density:1.2 
+	} 
+	ball = Bodies.circle(260,100,radius/2,ball_options); 
 	World.add(world,ball);
 
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-
-  
-  ground.display();
-  wall.display();
-  wall2.display();
-  drawSprites();
- 
-}
-
-function keypressed() {
-	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(ball,{x:0,y:0},{x:-0.05,y:-0.05});
-	}	
-}
-
-
+	groundObj=new Ground(width/2,670,width,20); 
+	leftSide = new Ground(1100,600,20,120); 
+	rightSide = new Ground(1350,600,20,120); 
+	Engine.run(engine); 
+} 
+function draw() { 
+	rectMode(CENTER); 
+	background(0); 
+	ellipse(ball.position.x,ball.position.y,radius,radius); 
+	groundObj.display(); 
+	leftSide.display(); 
+	rightSide.display(); 
+} 
+function keyPressed() { 
+	if (keyCode === UP_ARROW) { 
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85}); 
+	} }
